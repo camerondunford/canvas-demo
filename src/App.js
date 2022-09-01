61190db501b7e7d5995cf5a36690c9d22ce76119
Cameron Dunford <camerondunford@users.noreply.github.com>
2022-09-01 16:29:46 -0700

    Use `NetworkCanvas`
    Display a canvas that supports adding, connecting, & removing nodes.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
+import { NetworkCanvas } from "react-network-canvas";
+import { v1 as generateUuid } from "uuid";
+
+const onClickCanvas = (_event, position, graphManager) => {
+  // Create node at click position
+  const node = graphManager.createNode({
+    position,
+    inputPorts: [{ id: generateUuid() }],
+    outputPorts: [{ id: generateUuid() }],
+  });
+
+  // Select the newly created node
+  graphManager.selectedNodeIds = [node.id];
+};
+
+const onKeyPress = (_event, key, graphManager) => {
+  if (key === "Backspace" && graphManager.selectedNodeIds.length > 0) {
+    // Delete selected nodes
+    graphManager.removeNodesByIds(graphManager.selectedNodeIds);
+  }
+};
+
 
 function App() {
   return (
     <div className="App">
       <h1>Canvas Demo</h1>
+      <NetworkCanvas
+        onClickCanvas={onClickCanvas}
+        onKeyPress={onKeyPress}
+      />
     </div>
   );
 }
